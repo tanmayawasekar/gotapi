ee3973aea6b1ba5c57763624ac9bc8cbf08f1b89
Tanmay <tanmayawasekar@gmail.com>
2017-11-24 00:01:30 +0530

    made few changes in index

--- index.js
+++ index.js
@@ -1,10 +1,52 @@
-const express = require('express')
-const path = require('path')
-const PORT = process.env.PORT || 5000
+const express = require('express');
+const bodyParser = require('body-parser');
+const compression = require('compression');
+const helmet = require('helmet');
+const app = express();
+const routes = require('./routes/routes');
 
-express()
-  .use(express.static(path.join(__dirname, 'public')))
-  .set('views', path.join(__dirname, 'views'))
-  .set('view engine', 'ejs')
-  .get('/', (req, res) => res.render('pages/index'))
-  .listen(PORT, () => console.log(`Listening on ${ PORT }`))
+//Security Middleware
+app.use(helmet());
+
+app.set('port', 3000);
+
+require('./config/config');
+require('./config/dbConfig'); // Importing database connection when server starts
+
+app.use(function (req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, Authorization, X-Requested-With, Content-Type, Accept");
+  res.header("Access-Control-Allow-Methods", "POST, GET, OPTIONS, HEAD,PUT,DELETE");
+  if ('OPTIONS' === req.method) {
+    res.sendStatus(200);
+  } else {
+    next();
+  }
+});
+
+app.use(bodyParser.json());
+
+app.use(bodyParser.urlencoded({ 'extended': true }));
+
+//Using the compression middleware
+app.use(compression());
+
+//Handles routes in the app
+app.use('/api', routes);
+
+// When route is not found error messege is thrown
+app.use('/', function (req, res) {
+  res.status(404).send('Route Not Found');
+});
+
+// error handler
+app.use(function (err, req, res, next) {
+  res.status(err.status || 500).send(err.message || 'Internal Server Error');
+});
+
+//Best practices app settings
+app.set('query parser', `extended`);
+
+app.listen(app.get('port'), () => {
+  console.log(`Find the server at: http://localhost:${app.get('port')}/`);
+});
